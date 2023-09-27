7f78ba20c40bd711ee796a016cafdc9ec82aaf39
Aria Xiying Bao <87568028+ariaxxxi@users.noreply.github.com>
2023-09-27 15:23:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-from auth_token import auth_token
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-import torch
-from torch import autocast
-from diffusers import StableDiffusionPipeline
+from PIL import Image
 from io import BytesIO
+from auth_token import auth_token
 import base64
+import requests  
+import replicate
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+lora_url = "https://replicate.delivery/pbxt/lxNOXRnkcqYADhf5X6WUIQqzBGR0VhLY53JpTi0FuGInYRdIA/tmpqudyfillcartoon_datazip.safetensors"
 
 @app.get("/")
 def generate(text: str):
-    with autocast("cuda"):
-        image = pipe(text, guidance_scale=8.5).images[0]
+    try:
+        output = replicate.run(
+                "cloneofsimo/lora:fce477182f407ffd66b94b08e761424cabd13b82b518754b83080bc75ad32466", 
+                input={"prompt": text +'in the style of <1>' ,
+                    "lora_urls": lora_url})
 
-    image.save("image.png")
+        response  = requests.get(output[0])
+        image_data = response.content
+        image = Image.open(BytesIO(image_data))
+        image.save("image.png")
 
-    buffer = BytesIO()
-    image.save(buffer, format="PNG")
-    imgstr = base64.b64encode(buffer.getvalue())
+        buffer = BytesIO()
+        image.save(buffer, format="PNG")
+        imgstr = base64.b64encode(buffer.getvalue())
 
-    return Response(imgstr, media_type="image/png")   
+        return Response(imgstr, media_type="image/png")
+    
+    except Exception as e:
+        return {"error": str(e)}
+
